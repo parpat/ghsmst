8dc342a6c7e72d4f9a9bd410edaac48d455c0f6c
parth <parth.patel10@uoit.net>
2016-11-17 16:38:23 -0500

    start script changes

--- startContainers.sh
+++ startContainers.sh
@@ -4,5 +4,5 @@ do
 #gnome-terminal -x sh -c "docker run -d --rm --name ghs$i -P -v /home/parth/gocode/src/github.com/parpat/ghsmst/logs:/logs ghsmst; bash"
 #
 docker run -d --name ghs$i -P -v /home/parth/gocode/src/github.com/parpat/ghsmst/logs:/logs ghsmst
-sleep 1
+#sleep 0.1
 done
